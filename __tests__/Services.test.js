7ac9e253748d111f8a6e188eba6a311bc4231561
sidath <sidaath@gmail.com>
2022-04-17 11:23:53 +0530

    edit service tests

--- __tests__/Services.test.js
+++ __tests__/Services.test.js
@@ -1,23 +1,33 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import { SERVICES } from "../App/Model/Keys"
-import { addService, findService, getAllServices } from "../App/Model/Services"
+import { addService, editService, findService, getAllServices, removeService } from "../App/Model/Services"
 import ServiceObject from "../App/ViewModel/ServiceObject"
 
 
 describe('Calling async storage with proeper arguments', ()=>{
     const t = new ServiceObject("t1", "T1 DESC", true, false, null)
     const t2 = new ServiceObject("t2", "T2 DESC", true, false, null)
-    test('First service to be saved', async ()=>{
+    test('Save new service is called for the first time', async ()=>{
         const ar = [t]
         await addService(t)
         expect(AsyncStorage.setItem).toBeCalledWith(SERVICES, JSON.stringify(ar))
         AsyncStorage.clear()
     })
     
-    test('Data already exists', async ()=>{
+    test('Save new service is called when saved services already exist', async ()=>{
         const serviceArray = [t,t2]
         await addService(t)
         await addService(t2)
+        expect(AsyncStorage.setItem).toBeCalledWith(SERVICES, JSON.stringify(serviceArray))
+        AsyncStorage.clear()
+    })
+
+    test('Service is removed from a set of existing services.', async ()=>{
+        await addService(t)
+        await addService(t2)
+        const t2ID = t2.serviceID
+        await removeService(t2ID)
+        const serviceArray = [t]
         expect(AsyncStorage.setItem).toBeCalledWith(SERVICES, JSON.stringify(serviceArray))
         AsyncStorage.clear()
     })
@@ -63,5 +73,53 @@ describe('Check if data is saved properly', ()=>{
         AsyncStorage.clear()
     })
 
+})
 
+describe('Remove service removes correct services', ()=>{
+    const t1 = new ServiceObject("obj 1", "description 1", true, true, '2433')
+    const t2 = new ServiceObject("obj 2", "description 2", false, true, '2500')
+    const t3 = new ServiceObject("obj 3", "description 3", false, false, null)
+    const t4 = new ServiceObject("obj 4", "description 4", true, false, null)
+    const t5 = new ServiceObject("obj 5", "description 5", true, true, '23')
+
+    test('Correct array is saved to disk after filtering out service to remove', async ()=>{
+        await addService(t1)
+        await addService(t2)
+        await addService(t3)
+        await addService(t4)
+        await addService(t5)
+
+        await removeService(t1.serviceID)
+        const expectedArray  = [t2,t3,t4,t5]
+        expect(AsyncStorage.setItem).toBeCalledWith(SERVICES,JSON.stringify(expectedArray))
+    })
+
+    test('After removing services, other services are still there', async () =>{
+        const expectedArray  = [t2,t3,t4,t5]
+        const result = await getAllServices()
+        expect(result.services).toEqual(expectedArray)
+        AsyncStorage.clear()
+    })
+})
+
+describe('Edit service calls async storage with edited values.', ()=>{
+    test('Edited value is changed in the new array to be saved', async()=>{
+        await AsyncStorage.clear()
+        const t1 = new ServiceObject("x1", "description 1", true, true, '2433')
+        const t2 = new ServiceObject("x2", "description 2", false, true, '2500')
+        const t3 = new ServiceObject("x3", "description 3", false, false, null)
+        const t4 = new ServiceObject("x4", "description 4", true, false, null)
+        const t5 = new ServiceObject("x5", "description 5", true, true, '23')
+
+        await addService(t1)
+        await addService(t2)
+        await addService(t3)
+        await addService(t4)
+        await addService(t5)
+
+        const editedService = {...t4, serviceName : "Edited Name"}
+
+        res = await editService(editedService)
+        expect(res.index).toBe(3)
+    })
 })
---
+++ __tests__/ViewModel.test.js
@@ -0,0 +1,55 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import ServiceObject from "../App/ViewModel/ServiceObject"
+import { readAllServices,  saveEditedService,  saveNewService, searchServiceByName } from "../App/ViewModel/Services"
+
+describe('Checking service operations', ()=>{
+
+    test('Saving new service', async ()=>{
+        const res = await saveNewService("s1","desc",false,false,null)
+        expect(res.result).toBe(true)
+
+        const res2 = await saveNewService("s2","desc",false,false,null)
+        expect(res2.result).toBe(true)
+    })
+
+    test('saving service with existing name', async ()=>{
+        const res = await saveNewService("s1","desc",false,false,null)
+        expect(res.result).toBe(false)
+        expect(res.nameTaken).toBe(true)
+    })
+
+    test('reading all saved services', async()=>{
+        const ob1 = new ServiceObject("s1","desc",false,false,null)
+        const ob2 = new ServiceObject("s2","desc",false,false,null)
+        delete ob1.serviceID
+        delete ob2.serviceID
+
+        const res = await readAllServices()
+        expect(res.result).toBe(true)
+        expect(res.services[0]).toEqual(expect.objectContaining(ob1))
+        expect(res.services[1]).toEqual(expect.objectContaining(ob2))
+    })
+
+    test('searching a service by name', async ()=>{
+        const service = await searchServiceByName("s1")
+        expect(service.result).toBe(true)
+        expect(service.service.serviceName).toBe("s1")
+    })
+
+    test('editing existing service', async()=>{
+        const res = await searchServiceByName("s1")
+        const service = res.service
+        const editedService = {...service, serviceName : "s1 edited"}
+        const saveRes = await saveEditedService(editedService)
+        expect(saveRes.result).toBe(true)
+
+        const savedService = await searchServiceByName("s1 edited")
+        expect(savedService.result).toBe(true)
+        expect(savedService.service.serviceName).toBe("s1 edited")
+
+        await AsyncStorage.clear()
+    })
+
+})
+
+
