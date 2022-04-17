442b3461fa37e415abcd826a8fe278212335cc36
sidath <sidaath@gmail.com>
2022-04-17 11:22:58 +0530

    edit service, search service by name

--- App/ViewModel/Services.js
+++ App/ViewModel/Services.js
@@ -1,4 +1,4 @@
-import { addService, getAllServices } from "../Model/Services";
+import { addService, editService, findService, getAllServices, removeService } from "../Model/Services";
 import { removeAllServices } from "../Model/ServicesDev";
 import ServiceObject from "./ServiceObject";
 
@@ -22,6 +22,49 @@ export async function saveNewService(serviceName, serviceDescription, monthly, f
     return false
 }
 
+export async function saveEditedService(service){
+    try{
+        const editResponse = await editService(service)
+        if(editResponse.result === true){
+                return {result : true, savedIndex : editResponse.index}
+        }else{
+            if(editResponse.error) return {result : false, error : editResponse.error}
+        }
+    }catch(e){
+        console.error("VM Services.js, saveEditedService, catch : ",e)
+        return {result : false}
+    }
+}
+
+
+export async function readAllServices(){
+    try{
+        const data = await getAllServices()
+        if(data.result === true){
+            return {result : true, services : data.services}
+        }else{
+            if(data.error) return {result : false, error : data.error}
+        }
+        return {result : false}
+    }catch(e){
+        console.error("ViewModel, Services, getAllServices\n",e)
+        return {result : false}
+    }
+}
+
+export async function searchServiceByName(serviceName){
+    try{
+        const res = await findService(serviceName)
+        if(res.result === true){
+            return {result : true, service : res.service}
+        }else{
+            if(res.error) return {result : false, error : res.error}
+            return {result : false}
+        }
+    }catch(e){
+        console.error("Services VM, readServiceByName, catch ",e)
+    }
+}
 
 
 //dev
