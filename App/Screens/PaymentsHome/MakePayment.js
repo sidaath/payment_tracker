54ae2d7c02c2b3c45580ad59fcd10c789754e564
sidath <sidaath@gmail.com>
2022-04-24 22:57:12 +0530

    add pick month option

--- App/Screens/PaymentsHome/MakePayment.js
+++ App/Screens/PaymentsHome/MakePayment.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import {ScrollView} from 'react-native'
-import { Card, Text } from 'react-native-paper'
-
+import {Provider, Surface, Text, Title } from 'react-native-paper'
+import PickMonth from './PaymentOptions/PickMonth'
 
 export default function MakePayment({navigation, route}){
 
     const [service, setService] = React.useState(route.params?.service? route.params.service : null )
-    console.log(service)
+    const [month, setMonth] = React.useState('Pick Month')
 
     return (
+        <Provider>
         <ScrollView>
-            <Card>
-                <Card.Title title={`Details : ${service.serviceName}`} />
-                {service.monthly && <Text>Pick Month</Text>}
+        <Surface >
+            <Title>{service.serviceName}</Title>
+                {service.monthly && <PickMonth month={month} setMonth={setMonth}/>}
                 {!service.monthly && <Text>Pick date of payment</Text>}
                 {service.fixedAmount && <Text>Amount : {`${service.amount}`}</Text>}
                 {!service.fixedAmount && <Text>Enter Amount</Text>}
                 <Text>Payment Method</Text>
-            </Card>
+        </Surface>
         </ScrollView>
+        </Provider>
     )
 }
+
+
---
+++ App/Screens/PaymentsHome/PaymentOptions/PickMonth.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {StyleSheet, View} from 'react-native'
+import { Button,  Menu, Title } from 'react-native-paper'
+import { months } from '../../../ViewModel/Months'
+
+export default function PickMonth({month, setMonth}){
+    const [show, setShow] = React.useState(false)
+    const openMenu = () => setShow(true);
+
+    const selectMonth = (month) =>{
+        setShow(false)
+        setMonth(month)
+    }
+    return(
+        
+        <View style={styles.menuView}>
+            <Title>Month</Title>
+            <Menu
+                style={styles.menu} 
+                visible={show} 
+                onDismiss={()=>{setShow(false)}} 
+                anchor={<Button mode='outlined' onPress={openMenu}>{month}</Button>}>
+                {months.map((month)=>{
+                console.log(month)
+                return(
+                    <Menu.Item title={month} onPress={() => {selectMonth(month)}} key={month} />
+                )
+            })}
+        </Menu>
+        
+        </View>   
+    )
+}
+
+const styles = StyleSheet.create({
+    menu : {
+        top : 10,
+        left : 110,
+        right : 110
+    },
+
+    menuView : {
+        alignItems : 'center'
+    }
+})
